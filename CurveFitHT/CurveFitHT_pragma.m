5d3f87d3d6e7383be1c6f9d24606e00fe6afa50f
rdwijn <rdwijn>
2004-08-25 12:57:05 +0000

    no message

---
+++ CurveFitHT/CurveFitHT_pragma.m
@@ -0,0 +1,11 @@
+function CurveFitHT_pragma
+% this is a dummy function
+% only listing the fit functions 
+% for CurveFitHT: these are not explicitely named in CurveFitHT 
+% compiler pragma
+% list non explicit function in curvefitHT
+%#function cfExpAss
+%#function cfExpAssBnd
+%#function cfViniLin
+%#function cfViniP2
+%#function cfExpDec
