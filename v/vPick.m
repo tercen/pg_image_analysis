94f78cfb25388cc5df414c75adfa8354bdae2cdf
rdwijn <rdwijn>
2004-10-28 09:41:19 +0000

    no message

--- v/vPick.m
+++ v/vPick.m
@@ -1,7 +1,7 @@
 function clList = vPick(v, strID);
 % clList = vPick(v, strID)
 % SEE vArrange96, vMap96
-h = vPickGui(v, strID);
+h = vPickList(v, strID);
 waitfor(h, 'Visible');
 handles = guidata(h);
 delete(h);
