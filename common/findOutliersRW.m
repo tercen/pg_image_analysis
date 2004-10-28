adb9c04f36660ae266427bef9cdcd4f0180bef56
rdwijn <rdwijn>
2004-10-28 13:54:37 +0000

    no message

--- common/findOutliersRW.m
+++ common/findOutliersRW.m
@@ -1,27 +1,25 @@
-function [mOut, wSigma] = findOutliersRW(data, maxSigmaFactor, eps)
-iter = 0;
-maxIter = 5;
+function mOut = findOutliersRW(data, maxSigmaFactor, eps)
 sData = size(data);
-meanData    = mean(data);
+meanData    = mean(data)
 medianData  = median(data);
 K = 4.65;
 mOut = zeros(size(data));
-wSigma = zeros(1, sData(2));
 for i=1:sData(2)
+    M = abs(meanData(i)-medianData(i))/meanData(i)
     while(1)
-        iter = iter + 1;
         robVar = median(abs(data(:,i) - medianData(i) ))/0.6745;
         wRes =  ((data(:,i) - medianData(i)))/(K*robVar);
         wRob = zeros(size(wRes));
         iNonZero = find(wRes<1);
         wRob(iNonZero) = ( 1 - wRes(iNonZero).^2).^2;
-        wSigma(i) = weightedDeviation(data(:,i), medianData(i), wRob);
-        iOut = find( abs(data(:,i)-medianData(i)) > maxSigmaFactor * wSigma(i));
+        wSigma = weightedDeviation(data(:,i), medianData(i), wRob);
+        iOut = find( abs(data(:,i)-medianData(i)) > maxSigmaFactor * wSigma);
         mOut(iOut, i) = 1;
         meanData(i)     = mean  (data(find(~mOut(:,i))  ,i));
         medianData(i)   = median(data(find(~mOut(:,i))  ,i));
-        M = abs(meanData(i)-medianData(i))/meanData(i);
-        if (M < eps) | iter>maxIter
+        
+        M = abs(meanData(i)-medianData(i))/meanData(i)
+        if (M < eps)
             break;
         end
     end
--- qcModule/qcKinase.m
+++ qcModule/qcKinase.m
@@ -2,8 +2,26 @@ function [vOut, eCode] = qcKinase(v, settingsFile)
 vOut = [];
 eCode = 1;
 iniPars.refID = '';
+iniPars.maxSigmaFacRef = 4;
+iniPars.epsRef = 0.01;
+
 iniPars = getparsfromfile(settingsFile, iniPars);
+
+
+
 uniqueID    = vGetUniqueID(v, 'ID');
 refID       = strmatch(iniPars.refID, uniqueID);
-
 % do the ref spot statistics:
+% for now put all refspots on a single large stack
+refMaps = vAverage96(v, 'ID', refID, 'EndLevel');
+[s1,s2,s3] = size(refMaps);
+ref = reshape(s1*s2*s3,1);
+[iOut, wSigma] = findOutliersRW(ref, iniPars.maxSigmaRef, iniPars.epsRef);
+
+
+
+
+
+
+for i=1:length(v)
+    
