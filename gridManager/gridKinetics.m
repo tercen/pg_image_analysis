b4656aedc6734b7e27d9fa3043cea2dac36b9f57
rdwijn <rdwijn>
2005-06-23 07:06:28 +0000

    no message

--- gridManager/gridKinetics.m
+++ gridManager/gridKinetics.m
@@ -1,22 +1,23 @@
-function [x,y,oS, oQ] = gridKinetics(I, iGrid, oArray, pars, rSize, clID);
-
-axRot = [pars.minRot:pars.dRot:pars.maxRot];
-oArray = set(oArray, 'rotation', axRot);
-oPP = preprocess();
-oPP = set(oPP, 'nCircle',pars.nCircle, 'nLargeDisk', pars.nLargeDisk, 'nSmallDisk', pars.nSmallDisk, 'nBlurr', pars.nBlurr );
+function [x,y,oQ, oArray] = gridKinetics(I, iGrid, oArray, oPP, rSize, clID);
 
 Ipp = getPrepImage(oPP, I(:,:,iGrid));
 Ippr = imresize(Ipp, rSize);
 
-rPitch = pars.spotPitch * (rSize./size(Ipp));
-rSpotSize  = pars.spotSize * mean(rSize./size(Ipp));
-oArray = set(oArray, 'spotPitch', rPitch, 'spotSize', rSpotSize);
+
 
 [x,y,rotOut, oArray] = gridFind(oArray, histeq(Ippr));
 x = x* (size(I,1)/rSize(1));
 y = y* (size(I,2)/rSize(2));
 
-oS = segmentation(Ipp, x,y, 'areaSize', pars.spotPitch + 2);
+x(x<1) = 1;
+y(y<1) = 1;
+x(x>size(I,1)) = size(I,1);
+y(y>size(I,2)) = size(I,2);
+
+rPitch = get(oArray, 'spotPitch');
+spotPitch = rPitch(1) * (size(I,1)/rSize(1));
+
+oS = segmentation(Ipp, x,y, 'areaSize', spotPitch + 2);
 
 [nRows, nCols] = size(get(oArray, 'mask'));
 for j =1:size(I,3)
--- gridManager/gridManager.m
+++ gridManager/gridManager.m
@@ -22,7 +22,7 @@ function varargout = gridManager(varargin)
 
 % Edit the above text to modify the response to help gridManager
 
-% Last Modified by GUIDE v2.5 17-Jun-2005 22:30:19
+% Last Modified by GUIDE v2.5 21-Jun-2005 12:51:22
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -55,6 +55,7 @@ function gridManager_OpeningFcn(hObject, eventdata, handles, varargin)
 handles.iniFile = 'gridManager.ini';
 iniPars.dataDir = 'C:\';
 iniPars.templateDir = pwd;
+iniPars.resDir  = '_Quantified';
 iniPars.dftInstrument = 'detect';
 iniPars.xResize = 256;
 iniPars.yResize = 256;
@@ -68,11 +69,12 @@ handles.output = hObject;
 handles.oArray = [];
 handles.template = [];
 handles.oArray = array();
+handles.oP      = preProcess();
 set(gcf, 'position', [6.4 7.07692 95.4 29.3846]);
 % Update handles structure
 set(handles.pbAll, 'enable', 'off');
 set(handles.pbThis, 'enable', 'off');
-
+handles.version = 'alpha';
 
 
 guidata(hObject, handles);
@@ -100,23 +102,9 @@ function pbThis_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-instrumentFile = [handles.instrument,'.instrument'];
-try
-    instrumentPars = readInstrumentParameters(instrumentFile);
-catch
-    errordlg(lasterr);
-    return
-end
-names = fieldnames(instrumentPars);
-for i=1:length(names)
-    if isempty(instrumentPars.(names{i}))
-        errordlg('Instrument parameters were not properly initalized');
-        return;
-    end
-end
-handles.bShow = 1;
 
-handles = analyze(hObject, handles, instrumentPars);
+handles.bShow = 1;
+handles = analyze(hObject, handles);
 guidata(hObject, handles);
 
 
@@ -126,42 +114,101 @@ function pbAll_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+set(hObject, 'enable', 'off');
+if isequal(get(handles.miUpdate, 'checked'), 'off')
+    handles.bShow = 0;
+else
+    handles.bShow = 1;
+end
 
-% --------------------------------------------------------------------
-function miLoadSet_Callback(hObject, eventdata, handles)
-% hObject    handle to miLoadSet (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-newDir = uigetdir(handles.iniPars.dataDir, 'Please, select a data set.');
-if newDir
-    handles.iniPars.dataDir = newDir;
-    try
-        oData = dataSet('path', newDir, 'instrument', handles.iniPars.dftInstrument);
-        set(gcf, 'pointer', 'watch');
-        drawnow;
-        oData = getList(oData);
-        list = get(oData, 'list');
-        set(handles.stData, 'string', newDir);
-        
-        handles.list = list;
-        handles.instrument = get(oData, 'instrument');
-       
-    catch
-        set(gcf, 'pointer', 'arrow');
-        errstr = lasterr;
-        errordlg(lasterr,'Load Failed !!!');
+[nRows, nCols] = size(handles.arrays);
+for i=1:nRows
+    for j=1:nCols
+        if handles.arrays(i,j).used
+            well_Callback(handles.arrays(i,j).hPlot, [], handles);
+            handles.selectedWell = [i,j];
+            handles = analyze(hObject, handles);
+            drawnow
+        end
     end
 end
-arrays = initializeDataSet(list, handles.instrument);
-arrays = initializeGraph(handles.axes1, arrays);
-handles.arrays = arrays;
-set(gcf, 'pointer', 'arrow');
-drawnow;
-guidata(hObject, handles); 
-  
-        
-  
+set(hObject, 'enable', 'on');
 
+
+
+guidata(hObject, handles);
+
+% --------------------------------------------------------------------
+    function miLoadSet_Callback(hObject, eventdata, handles)
+        % hObject    handle to miLoadSet (see GCBO)
+        % eventdata  reserved - to be defined in a future version of MATLAB
+        % handles    structure with handles and user data (see GUIDATA)
+        newDir = uigetdir(handles.iniPars.dataDir, 'Please, select a data set.');
+        if newDir
+            handles.iniPars.dataDir = newDir;
+            try
+                oData = dataSet('path', newDir, 'instrument', handles.iniPars.dftInstrument);
+                set(gcf, 'pointer', 'watch');
+                drawnow;
+                oData = getList(oData);
+                list = get(oData, 'list');
+                set(handles.stData, 'string', newDir);
+
+                handles.list = list;
+                handles.instrument = get(oData, 'instrument');
+                imSize = get(oData, 'imageSize');
+
+            catch
+                set(gcf, 'pointer', 'arrow');
+                errstr = lasterr;
+                errordlg(lasterr,'Load Failed !!!');
+                return
+            end
+            % read instrument parameters and initialize array object
+            instrumentFile = [handles.instrument,'.instrument'];
+            try
+                pars = readInstrumentParameters(instrumentFile);
+            catch
+                errordlg(lasterr);
+                return
+            end
+            names = fieldnames(pars);
+            for i=1:length(names)
+                if isempty(pars.(names{i}))
+                    errordlg('Instrument parameters were not properly initalized');
+                    return;
+                end
+            end
+
+            % take image resize into account!
+            rSize = [handles.iniPars.xResize, handles.iniPars.yResize];
+            rPitch     = pars.spotPitch * (rSize./imSize);
+            rSpotSize  = pars.spotSize * mean(rSize./imSize);
+
+
+            handles.oArray = set(handles.oArray, 'spotPitch', rPitch, 'spotSize', rSpotSize);
+            axRot = [pars.minRot:pars.dRot:pars.maxRot];
+            handles.oArray = set(handles.oArray, 'rotation', axRot);
+            handles.oP = set(handles.oP, 'nCircle',pars.nCircle, 'nLargeDisk', pars.nLargeDisk, 'nSmallDisk', pars.nSmallDisk, 'nBlurr', pars.nBlurr );
+
+            arrays = initializeDataSet(list, handles.instrument);
+            arrays = initializeGraph(handles.axes1, arrays);
+
+            handles.arrays = arrays;
+
+            well_Callback(arrays(1,1).hPlot, [], handles);
+            handles.selectedWell = [1,1];
+            set(gcf, 'pointer', 'arrow');
+            drawnow;
+
+            strDir = [handles.iniPars.dataDir, '\', handles.iniPars.resDir];
+            if ~exist(strDir, 'dir')
+                mkdir(handles.iniPars.dataDir, handles.iniPars.resDir);
+            end
+
+        end
+
+        guidata(hObject, handles);
 
 % --------------------------------------------------------------------
 function miData_Callback(hObject, eventdata, handles)
@@ -191,7 +238,8 @@ function miAbout_Callback(hObject, eventdata, handles)
 % hObject    handle to miAbout (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
+str = ['gridManager ', handles.version, ' (c)2005 PamGene International BV']; 
+helpdlg(str, 'About ...');
 function well_Callback(hObject, eventData, handles)
 wells = handles.arrays;
 
@@ -203,7 +251,7 @@ end
 if ~isempty(handles.selectedWell)
     iOld  = handles.selectedWell(1);
     jOld  = handles.selectedWell(2);
-    if isempty(wells(iOld, jOld).oS)
+    if ~wells(iOld, jOld).done
         strColor = 'k';
     else
         strColor = 'g';
@@ -247,10 +295,11 @@ switch instrument
                 if ~isempty(strmatch(strID, wList))
                     arrays(i,j).used = 1;
                     arrays(i,j).id = strID;
-                    arrays(i,j).oS   = [];
-                    arrays(i,j).oQ   = [];
+                    arrays(i,j).done = 0;
+                    arrays(i,j).strResultFile = [];
                 else
                     arrays(i,j).used = 0;
+                    arrays(i,j).done = 0;
                 end
             end
         end
@@ -264,10 +313,14 @@ switch instrument
             if ~isempty(strmatch(strID, wList));
                 arrays(i).used = 1;
                 arrays(i).id = strID;
-                arrays(i).oS = [];
-                arrays(i).oQ = [];
+                arrays(i).done = 0;
+                arrays(i).strResultFile = [];
+                
+
             else
                 arrays(i).used = 0;
+                arrays(i).done = 0;
+                
             end
         end
         
@@ -358,7 +411,10 @@ function miLoadTemplate_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 curDir = pwd;
-cd(handles.iniPars.templateDir);
+if exist(handles.iniPars.templateDir)
+    cd(handles.iniPars.templateDir);
+end
+    
 [fName, pathName] = uigetfile('*.txt', 'Please, select a template file');
 cd(curDir);
 if fName
@@ -376,7 +432,7 @@ end
 
 guidata(hObject, handles);
     
-function handles = analyze(hObject, handles, instrumentPars);
+function handles = analyze(hObject, handles);
 
 set(gcf, 'pointer', 'watch');
 drawnow
@@ -386,50 +442,129 @@ currentArray = handles.arrays(handles.selectedWell(1), handles.selectedWell(2));
 iCurrent = strmatch(currentArray.id, wellList); 
 currentList = handles.list(iCurrent);
 nImages = length(currentList);
-stString = ['Loading ',num2str(nImages), ' for well ',currentArray.id];
+stString = ['Analyzing ',num2str(nImages), ' images for well ',currentArray.id,'...'];
 set(handles.stStatus, 'String', stString);
 drawnow
 rSize = [handles.iniPars.xResize, handles.iniPars.yResize];
 try
     for i=1:nImages
+        
         I(:,:,i) = imread([currentList(i).path, '\', currentList(i).name]);
-        pump  = char(currentList(i).P);
-        c(i) = str2num(pump(2:end));
-    end
+     end
 catch
     errordlg(lasterr, currentArray.id);
     return
 end
-[c, iSort] = sort(c);
-I = I(:,:,iSort);
 
-set(handles.stStatus, 'String', 'Analyzing');
 drawnow;
 time = clock;
 switch handles.gridMode
 
     case 'kinLast'
-        try 
-            [x,y,oS, oQ] = gridKinetics(I, nImages, handles.oArray, instrumentPars, rSize, handles.clID);
-        catch
-            errordlg(lasterr);
-            set(gcf, 'pointer', 'arrow');
-            return;
-        end
+%        try 
+            expMode = 'kinetics';
+            for i = 1:nImages
+                   pump  = char(currentList(i).P);
+                   c(i) = str2num(pump(2:end));
+                   str = fnReplaceExtension([currentList(i).path, '\', currentList(i).name], 'txt');
+                   expNames(i) = cellstr(str); 
+            end
+            [c, iSort] = sort(c);
+            I = I(:,:,iSort);
+             expNames = expNames(iSort);
+            
+            [x,y,oQ, handles.oArray] = gridKinetics(I, nImages, handles.oArray, handles.oP, rSize, handles.clID);
+           
+            
+%         catch
+%             errordlg(lasterr);
+%             set(gcf, 'pointer', 'arrow');
+%            return;
+   %     end
         
         case 'kinFirst'
+         try 
+             expMode = 'kinetics';
+             for i = 1:nImages
+                   pump  = char(currentList(i).P);
+                   c(i) = str2num(pump(2:end));
+                   expNames{i} = [currentList(i).path, '\', currentList(i).name];
+            end
+            [c, iSort] = sort(c);
+            I = I(:,:,iSort);
+            expNames = expNames(iSort);
+          
+            [x,y,oQ, handles.oArray] = gridKinetics(I, 1, handles.oArray, handles.oP, rSize, handles.clID);
         
-end
-currentArray.oQ = oQ;
-currentArray.oS = oS;
-handles.arrays(handles.selectedWell(1), handles.selectedWell(2)) = currentArray;
+         catch
+            errordlg(lasterr);
+            set(gcf, 'pointer', 'arrow');
+           return;
+        end
 
+
+end
+
+
+
+
+
+%qSave(oQ, currentArray.strResultFile);
+
+currentArray.done = 1;
+handles.arrays(handles.selectedWell(1), handles.selectedWell(2)) = currentArray;
 set(gcf, 'pointer', 'arrow');
 stString = ['Ready (',num2str(etime(clock, time)),'s)'];
 set(handles.stStatus, 'String', stString);
 if handles.bShow
-    hp = presenter(I,x,y,oS,oQ,c);
+    hp = presenter(I,x,y,oQ,c);
     set(hp, 'name', currentArray.id);
 end
+strBase = [currentList(i).W, '_', currentList(i).F, '_', currentList(i).T];
+resBase = [handles.iniPars.dataDir,'\', handles.iniPars.resDir, '\', strBase];
+exportWell(expNames, resBase, expMode, c, oQ);
+% --------------------------------------------------------------------
+function miUpdate_Callback(hObject, eventdata, handles)
+% hObject    handle to miUpdate (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if isequal(get(hObject, 'checked'), 'on')
+    set(hObject, 'checked', 'off');
+else
+    set(hObject, 'checked', 'on');
+end
 
+function exportWell(imNames, aggrName, aggrMode, aggrAxis, oQ)
+export(oQ, 'images', imNames);
+export(oQ, aggrMode, aggrName,aggrAxis);
+
+
+% --------------------------------------------------------------------
+function miExport_Callback(hObject, eventdata, handles)
+% hObject    handle to miExport (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function miExportImages_Callback(hObject, eventdata, handles)
+% hObject    handle to miExportImages (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if isequal(get(hObject, 'checked'), 'on')
+    set(hObject, 'checked', 'off');
+else
+    set(hObject, 'checked', 'on');
+end
+
+% --------------------------------------------------------------------
+function miExportQuantified_Callback(hObject, eventdata, handles)
+% hObject    handle to miExportQuantified (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if isequal(get(hObject, 'checked'), 'on')
+    set(hObject, 'checked', 'off');
+else
+    set(hObject, 'checked', 'on');
+end
 
