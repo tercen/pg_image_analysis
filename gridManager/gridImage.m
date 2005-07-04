88dab82cfcd850321ca18b66b8a916599dab68a0
rdwijn <rdwijn>
2005-07-04 05:13:40 +0000

    no message

---
+++ gridManager/gridImage.m
@@ -0,0 +1,19 @@
+function [Igrid, Isegment] = gridImage(I, oPP, ppMode, rSize)
+
+switch ppMode
+    case 'none'
+        Igrid = imresize(I, rSize);
+        Isegment = I;
+    case 'fast'
+        oPP = rescale(oPP, rSize(1)/size(I,1));
+        Igrid = getPrepImage(oPP, imresize(I, rSize));
+        Isegment = I;
+    case 'slow'
+        Igrid = getPrepImage(oPP, I);
+        Isegment = imresize(Ipp, rSize);
+end
+Igrid = histeq(Igrid);
+
+        
+   
+        
